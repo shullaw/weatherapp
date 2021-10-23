0aaa9ee44849cad06b6b30bc62c8d6e5c44b1b76
Shullaw <c00161818@louisiana.edu>
2021-10-23 12:51:52 -0500

    "Altered staticfiles dir in order to load images"

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,7 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+##-- Add additional image directory to static files (for issue #29) --##
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
