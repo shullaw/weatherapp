786461ff1a7e1467b7c58ac3bce2ef05714beedb
shullaw <c00161818@louisiana.edu>
2021-11-16 01:35:10 -0600

    m'ais las

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,12 +39,12 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
     # Add newly defined Django apps here
     'weatherapp',
     # 'bootstrap4',
