75364cbbc6d2e5d95c0086c0d74cdd60f3208455
Shullaw <c00161818@louisiana.edu>
2021-11-17 16:37:15 -0600

    removing staticURLS from settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,11 +141,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 ##-- Add additional image directory to static files (for issue #29) --##
 # FOR DEVELOPMENT #
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'weatherapp','static'),
-    os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = [
+#     # os.path.join(BASE_DIR, 'weatherapp','static'),
+#     os.path.join(BASE_DIR, 'static'),
 
-]
+# ]
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
